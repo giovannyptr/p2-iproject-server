0e252a4e6521a4ef2c427a97d38d5ae85be21df3
giovannyptr <giovannyputri@ymail.com>
2021-10-20 03:06:50 +0700

    setup app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 const routes = require('./routes/index')
 
-//bodyparser
+//bodyparser or middleware
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
